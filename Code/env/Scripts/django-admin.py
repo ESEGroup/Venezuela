b51d1c6aa91f948a1e1ea376204a84658b5add33
Marielen Ferreira <marielenmf@poli.ufrj.br>
2016-11-11 08:47:57 -0200

    Configurando Django #8

---
+++ Code/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\marielen\documents\engenharia de software\venezuela\code\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
