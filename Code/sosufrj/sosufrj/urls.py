c468504e70eaa87a0b4fac9e064f01abb9dbee1e
Marielen Ferreira <marielenmf@poli.ufrj.br>
2016-11-11 22:33:38 -0200

    Atualizando Planejamento Release - UC02 #12 - UC03 #13

--- Code/sosufrj/sosufrj/urls.py
+++ Code/sosufrj/sosufrj/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^cadastro_ocorrencia/', include('cadastro_ocorrencia.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
